aaeffe0a91ea7e607f2a2878ebed32ce62a5421a
Kjetil Brakstad <kjbrak@equinor.com>
2022-12-04 11:36:35 +0100

    add day4

---
+++ day4/day4.py
@@ -0,0 +1,11 @@
+data = open('day4/in4', 'r').read().split('\n')
+
+# Puzzle1
+l = [tuple(s.replace('-',',').split(',')) for s in data]
+lst = [tuple(int(s) for s in li) for li in l]
+crit = [i for i,tup in enumerate(lst) if ((tup[0]>=tup[2] and tup[1]<=tup[3]) | (tup[2]>=tup[0] and tup[3]<=tup[1])) ]
+print(f'Result1: {len(crit)}')
+
+# Puzzle2
+c = [i for i,tup in enumerate(lst) if ((tup[1]>=tup[2] and tup[0]<=tup[3]) | (tup[3]>=tup[0] and tup[2]<=tup[1])) ]
+print(f'Result2: {len(c)}')
