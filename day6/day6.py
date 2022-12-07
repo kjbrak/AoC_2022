d9390a7a88e315940c3cb9ef9ef83dace9e5513c
Kjetil Brakstad <kjbrak@equinor.com>
2022-12-07 10:57:48 +0100

    minor refactor in day6

--- day6/day6.py
+++ day6/day6.py
@@ -1,15 +1,16 @@
 data = open('day6/in6', 'r').read()
 
 
-def find_markers(data, sig_len):
-    markers = []
+def find_marker(data, sig_len):
     for i,s in enumerate(data):
         if i>=sig_len-1:
             sample = data[i-(sig_len-1):i+1]
             if len(set(sample)) == len(sample):
-                markers.append(i+1) # 1-indexed
-    return markers
+                return i+1
+
+    return None
 
 
-print(f"Result1: {find_markers(data, 4)[0]}")
-print(f"Result2: {find_markers(data, 14)[0]}")
+print(f"Result1: {find_marker(data, 4)}")
+print(f"Result2: {find_marker(data, 14)}")
+
